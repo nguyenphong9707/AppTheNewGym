1502c3a1c909b876077626b1affc3942fe9ecb42
nguyenphong9707 <nguyentphong9707@gmail.com>
2019-09-09 20:43:56 +0700

    update

--- component/Members/ModalMember.js
+++ component/Members/ModalMember.js
@@ -13,7 +13,7 @@ export default class ModalMember extends PureComponent {
                 </View>
                 <View style={{flex:0.1, alignItems:'center', paddingRight:10}}>
                 <Text style={{color: 'gray'}}>Vào lúc: {this.props.timebd.slice(0,5)} - Đến: {this.props.timekt.slice(0,5)}</Text>
-                    <Text>Ngày : {this.props.Ngay}</Text>
+                    <Text style={{color: 'gray'}}>Ngày : {this.props.Ngay}</Text>
                 </View>
                 <View style={{flex:0.8, alignItems:'center', padding:10, marginTop:15}}>
                     <Text style={{color: 'gray'}}>&nbsp;{this.props.noidung}</Text>
